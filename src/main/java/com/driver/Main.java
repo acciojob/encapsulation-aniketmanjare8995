ea263fc248c3a32bcd606e622ce1036aea2a20a9
Aniket Manjare <aniketmanjare8712@gmail.com>
2024-12-10 10:56:43 +0530

    encapsulation assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly rwOnly= new RWOnly();
+       // rwOnly.name= "Aniket";
+        //'name' has private access in 'com. driver. RWOnly'
+
+        rwOnly.setName("Aniket");
+        System.out.println(rwOnly.getName());
+    }
   
 }
